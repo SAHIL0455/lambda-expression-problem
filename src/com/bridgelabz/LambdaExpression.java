10d8e518d1926b11e4746f4957238644e97fad70
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-06 20:02:14 +0530

    ADDED: Show the Result Lambda function

--- src/com/bridgelabz/LambdaExpression.java
+++ src/com/bridgelabz/LambdaExpression.java
@@ -1,5 +1,8 @@
 package com.bridgelabz;
 interface MathOperation{
+     static void printresult(int a, int b, String function, MathOperation object){
+         System.out.println("Result of : "+function+" is "+object.calculate(a,b));
+    }
     int calculate(int a,int b);
 }
 public class LambdaExpression {
@@ -9,8 +12,8 @@ public class LambdaExpression {
         MathOperation sub = (int a, int b) -> a-b;
         MathOperation div = (int a, int b) -> a/b;
 
-        System.out.println("Addition "+add.calculate(5,8));
-        System.out.println("Subtraction "+sub.calculate(78,56));
-        System.out.println("Division "+div.calculate(55,5));
+        MathOperation.printresult(5,9,"Addition",add);
+        MathOperation.printresult(15,5,"Subtraction",sub);
+        MathOperation.printresult(25,5,"Division",div);
     }
 }
