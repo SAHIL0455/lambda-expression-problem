edefcb13239baf753712b25a7249ec3e0dd4f269
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-06 19:22:13 +0530

    INITIAL: messages

---
+++ src/com/bridgelabz/LambdaExpression.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class LambdaExpression {
+    public static void main(String[] args) {
+        System.out.println("Welcome the Lambda Expression Problems");
+    }
+}
