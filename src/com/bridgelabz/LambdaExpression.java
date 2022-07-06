849347c296be025dc3abd450e3f57fc45126833b
SAHIL0455 <sahilpawar1004@gmail.com>
2022-07-06 19:40:08 +0530

    ADDED: Develop Math Operation App to Perform Math Functions

--- src/com/bridgelabz/LambdaExpression.java
+++ src/com/bridgelabz/LambdaExpression.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
-
+interface MathOperation{
+    int calculate(int a,int b);
+}
 public class LambdaExpression {
     public static void main(String[] args) {
         System.out.println("Welcome the Lambda Expression Problems");
+        MathOperation add = (int a, int b) -> a+b;
+        MathOperation sub = (int a, int b) -> a-b;
+        MathOperation div = (int a, int b) -> a/b;
+
+        System.out.println("Addition "+add.calculate(5,8));
+        System.out.println("Subtraction "+sub.calculate(78,56));
+        System.out.println("Division "+div.calculate(55,5));
     }
 }
